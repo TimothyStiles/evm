089a78e547aacb47e63511ab747afab6ae681a4f
Timothy Stiles <timothysstiles@gmail.com>
2017-06-01 02:15:56 -0400

    added get_vid_dimensions function.

--- evm/utils.py
+++ evm/utils.py
@@ -2,12 +2,14 @@ import numpy as np
 import cv2 as cv
 
 def get_fps(path):
+    """Gets frames per second of video."""
     video = cv.VideoCapture(path)
     fps = video.get(cv.CAP_PROP_FPS)
     video.release()
     return fps
 
 def get_vid_height(path):
+    """Gets pixel height of video frames."""
     video = cv.VideoCapture(path)
     height = video.get(cv.CAP_PROP_FRAME_HEIGHT)
     video.release()
@@ -15,12 +17,22 @@ def get_vid_height(path):
 
 def get_vid_width(path):
     video = cv.VideoCapture(path)
+    """Gets pixel width of video frames."""
     width = video.get(cv.CAP_PROP_FRAME_WIDTH)
     video.release()
     return width
 
 def get_vid_length(path):
+    """Gets number of frames in video."""
     video = cv.VideoCapture(path)
     length = video.get(cv.CAP_PROP_FRAME_COUNT)
     video.release()
     return width
+
+def get_vid_dimensions(path):
+    """Gets dimensions of video."""
+    width = get_vid_width(path)
+    height = get_vid_height(path)
+    length = get_vid_length(path)
+    fps = get_fps(path)
+    return width, height, length, fps
