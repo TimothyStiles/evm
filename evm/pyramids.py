e717f98bc84fe39a19a3b238eaa412e50b32f3f4
Timothy Stiles <timothysstiles@gmail.com>
2017-06-04 14:42:07 -0400

    added video_laplacian function that returns pyramid as list of videos.

--- evm/pyramids.py
+++ evm/pyramids.py
@@ -1,6 +1,8 @@
 import numpy as np
 import cv2 as cv
 
+import evm.utils as utils
+
 def image_gaussian(img):
     """Computes image gaussian pyramid from frame or image."""
     gaussian = img.copy() #needs better name
@@ -25,3 +27,21 @@ def video_gaussian(path):
 
 def video_laplacian(path):
     """Computes video laplacian pyramid from video."""
+    video = utils.read_video(path)
+    laplacian = list()
+
+    for im in video:
+        laplacian.append(np.array(image_laplacian(im)))
+
+    laplacian = np.array(laplacian)
+    video_length = len(laplacian) - 1
+    pyramid_height = len(laplacian[0]) - 1
+    pyramid_videos = list()
+
+    for vid in range(pyramid_height, 0, -1):
+        frames = list()
+        for im in range(video_length):
+            frames.append(laplacian[:, vid][im])
+        pyramid_videos.append(np.array(frames))
+
+    return pyramid_videos
