eb57bf9d67e49b34f05d57b131b3a1516f6ed8f6
Timothy Stiles <timothysstiles@gmail.com>
2017-06-08 19:01:30 -0400

    cleaned up and organized test.py.

--- test.py
+++ test.py
@@ -9,6 +9,11 @@ import evm.utils as utils
 import evm.pyramids as pyramids
 import evm.filters as filters
 
+test_image_input_path = 'resources/test_input/slowpoke.png'
+test_video_input_path = 'resources/test_input/baby.mp4'
+test_output_path = 'resources/test_output/'
+
+
 class TestTruth(unittest.TestCase):
 
     def test_mult(self):
@@ -32,108 +37,116 @@ class TestCv(unittest.TestCase):
         self.assertTrue(callable(cv.imread))
 
     def test_imread(self):
-        img = cv.imread('resources/slowpoke.png')
+        img = cv.imread(test_image_input_path)
         px = img[0, 0, 0]
         self.assertTrue(px == 0)
 
     def test_imwrite(self):
-        img = cv.imread('resources/slowpoke.png')
+        img = cv.imread(test_image_input_path)
         gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-        cv.imwrite('resources/slowpoke-gray.png', gray)
-        self.assertTrue(os.path.isfile('resources/slowpoke-gray.png'))
+        cv.imwrite(test_output_path + 'slowpoke-gray.png', gray)
+        self.assertTrue(os.path.isfile(test_output_path + 'slowpoke-gray.png'))
 
     def test_video_identity(self):
         self.assertTrue(callable(cv.VideoCapture))
 
     def test_videocapture(self):
-        cap = cv.VideoCapture('resources/baby.mp4')
+        cap = cv.VideoCapture(test_video_input_path)
         ret, frame = cap.read()
         px = frame[0, 0, 0]
         cap.release()
         self.assertTrue(px == 7)
 
+
+class TestUtils(unittest.TestCase):
+
     def test_get_vid_height(self):
-        self.assertTrue(utils.get_vid_height('resources/baby.mp4') == 544.0)
+        self.assertTrue(utils.get_vid_height(test_video_input_path) == 544.0)
 
     def test_get_vid_width(self):
-        self.assertTrue(utils.get_vid_width('resources/baby.mp4') ==  960.0)
+        self.assertTrue(utils.get_vid_width(test_video_input_path) ==  960.0)
 
     def test_get_fps(self):
-        self.assertTrue(utils.get_fps('resources/baby.mp4') == 30.0)
+        self.assertTrue(utils.get_fps(test_video_input_path) == 30.0)
 
     def test_get_length(self):
-        self.assertTrue(utils.get_vid_length('resources/baby.mp4') == 301.0)
+        self.assertTrue(utils.get_vid_length(test_video_input_path) == 301.0)
 
     def test_get_dimensions(self):
-        width, height, length, fps = utils.get_vid_dimensions('resources/baby.mp4')
+        width, height, length, fps = utils.get_vid_dimensions(test_video_input_path)
         self.assertTrue(width == 960.0 and height == 544.0 and length == 301.0 and fps == 30.0)
 
-    def test_gaussian_image_pyramid(self):
-        img = cv.imread('resources/slowpoke.png')
-        pyramid = pyramids.gaussian_image_pyramid(img)
-        utils.save_image_pyramid(pyramid, 'resources/slowpoke-gaussian-pyramid.png')
-
-    def test_laplacian_image_pyramid(self):
-        img = cv.imread('resources/slowpoke.png')
-        pyramid = pyramids.laplacian_image_pyramid(img)
-        utils.save_image_pyramid(pyramid, 'resources/slowpoke-laplacian-pyramid.png')
-
     def test_read_stream(self):
-        video = utils.read_video('resources/baby.mp4')
-        video2 = utils.read_video('resources/baby.mp4')
+        video = utils.read_video(test_video_input_path)
+        video2 = utils.read_video(test_video_input_path)
 
     def test_read_video(self):
-        video = utils.read_video('resources/baby.mp4')
+        video = utils.read_video(test_video_input_path)
         length, height, width, pixel = video.shape
         self.assertTrue(width == 960 and height == 544 and length == 301 and pixel == 3)
 
     def test_write_video(self):
-        video = utils.read_video('resources/baby.mp4')
-        fps = utils.get_fps('resources/baby.mp4')
-        utils.write_video(video,'./resources/baby-write-test.avi',fps)
-        video_test = utils.read_video('resources/baby-write-test.avi')
+        video = utils.read_video(test_video_input_path)
+        fps = utils.get_fps(test_video_input_path)
+        utils.write_video(video, test_output_path + 'baby-write-test.mp4',fps)
+        video_test = utils.read_video(test_output_path + 'baby-write-test.mp4')
         length, height, width, pixel = video_test.shape
         self.assertTrue(width == 960 and height == 544 and length == 301 and pixel == 3)
 
+
+class TestPyramids(unittest.TestCase):
+
+    def test_gaussian_image_pyramid(self):
+        img = cv.imread(test_image_input_path)
+        pyramid = pyramids.gaussian_image_pyramid(img)
+        utils.save_image_pyramid(pyramid, test_output_path + 'slowpoke-gaussian-pyramid.png')
+
+    def test_laplacian_image_pyramid(self):
+        img = cv.imread(test_image_input_path)
+        pyramid = pyramids.laplacian_image_pyramid(img)
+        utils.save_image_pyramid(pyramid, test_output_path + 'slowpoke-laplacian-pyramid.png')
+
     def test_gaussian_video_pyramid(self):
-        path = 'resources/baby.mp4'
+        path = test_video_input_path
         fps = utils.get_fps(path)
         laplacians = pyramids.gaussian_video_pyramid(path)
-        utils.write_video(laplacians[0], 'resources/baby-gaussian.mp4', fps)
+        utils.write_video(laplacians[0], test_output_path + 'baby-gaussian.mp4', fps)
 
     def test_laplacian_video_pyramid(self):
-        path = 'resources/baby.mp4'
+        path = test_video_input_path
         fps = utils.get_fps(path)
         laplacians = pyramids.laplacian_video_pyramid(path)
-        utils.write_video(laplacians[0], 'resources/baby-laplacian.mp4', fps)
-
-    def test_temporal_bandpass_filter(self):
-        path = 'resources/baby.mp4'
-        pyramid = pyramids.laplacian_video_pyramid(path)
-        fps = int(utils.get_fps(path))
-        filtered_video= filters.temporal_bandpass_filter(pyramid[3], fps)
-        utils.write_video(filtered_video, 'resources/baby-time-filtered.mp4', fps)
+        utils.write_video(laplacians[0], test_output_path + 'baby-laplacian.mp4', fps)
 
     def test_collapse_image_pyramid(self):
-        img = cv.imread('resources/slowpoke.png')
+        img = cv.imread(test_image_input_path)
         pyramid = pyramids.laplacian_image_pyramid(img)
         collapsed_pyramid = pyramids.collapse_image_pyramid(pyramid)
-        cv.imwrite('resources/slowpoke-pyramid-collapse.png', collapsed_pyramid)
+        cv.imwrite(test_output_path + 'slowpoke-pyramid-collapse.png', collapsed_pyramid)
 
     def test_collapse_video_pyramid(self):
-        path = 'resources/baby.mp4'
+        path = test_video_input_path
         fps = utils.get_fps(path)
         laplacian_pyramid = pyramids.laplacian_video_pyramid(path)
         collapsed_pyramid = pyramids.collapse_video_pyramid(laplacian_pyramid)
-        utils.write_video(collapsed_pyramid, 'resources/baby-collapse-video-pyramid.mp4', fps)
+        utils.write_video(collapsed_pyramid, test_output_path + 'baby-collapse-video-pyramid.mp4', fps)
+
+class TestFilters(unittest.TestCase):
+
+    def test_temporal_bandpass_filter(self):
+        path = test_video_input_path
+        pyramid = pyramids.laplacian_video_pyramid(path)
+        fps = int(utils.get_fps(path))
+        filtered_video= filters.temporal_bandpass_filter(pyramid[3], fps)
+        utils.write_video(filtered_video, test_output_path + 'baby-time-filtered.mp4', fps)
 
     def test_filter_video_pyramid(self):
-        path = 'resources/baby.mp4'
+        path = test_video_input_path
         fps = utils.get_fps(path)
         laplacian_pyramid = pyramids.laplacian_video_pyramid(path)
-        filtered_pyramid = filters.filter_video_pyramid(laplacian_pyramid, fps)
-        collapsed_pyramid = pyramids.collapse_video_pyramid(filtered__pyramid)
-        utils.write_video(collapsed_pyramid, 'resources/baby-collapse-filtered-video-pyramid.mp4', fps)
+        filtered_pyramid = filters.filter_video_pyramid(laplacian_pyramid[1:-1], fps, 0.4, 3, 0, 10)
+        collapsed_pyramid = pyramids.collapse_video_pyramid(filtered_pyramid)
+        utils.write_video(collapsed_pyramid, test_output_path + 'baby-collapse-filtered-video-pyramid.mp4', fps)
 
 class TestRequests(unittest.TestCase):
 
